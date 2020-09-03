6510f6bd30859f65da4185b3ea209bb624b209fe
tomasmedina <tomasmedina.a@gmail.com>
2020-09-02 23:01:51 -0400

    Updates

---
+++ precache-manifest.685f7796db8be6a6d10cd2377113473c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e153cac855c8bb36293c5cf9a755087",
+    "url": "/leniolabscodechallenge/index.html"
+  },
+  {
+    "revision": "d7bb8e701840466ab3f5",
+    "url": "/leniolabscodechallenge/static/css/main.1787c169.chunk.css"
+  },
+  {
+    "revision": "346ce8a4686ae78f2f44",
+    "url": "/leniolabscodechallenge/static/js/2.b6139b73.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/leniolabscodechallenge/static/js/2.b6139b73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7bb8e701840466ab3f5",
+    "url": "/leniolabscodechallenge/static/js/main.34fa062a.chunk.js"
+  },
+  {
+    "revision": "144b578dd2e5edc57380",
+    "url": "/leniolabscodechallenge/static/js/runtime-main.dcdd6bbd.js"
+  },
+  {
+    "revision": "3ae13bc863b5aebd90257119aa4b22f0",
+    "url": "/leniolabscodechallenge/static/media/facebook.3ae13bc8.svg"
+  },
+  {
+    "revision": "a7a925c7c07f163f3274df0c1ab2c321",
+    "url": "/leniolabscodechallenge/static/media/twitter.a7a925c7.svg"
+  },
+  {
+    "revision": "3089292a89d1ef15d3c4c58f62958f48",
+    "url": "/leniolabscodechallenge/static/media/youtube.3089292a.svg"
+  }
+]);
